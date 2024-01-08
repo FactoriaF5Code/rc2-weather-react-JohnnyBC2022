c326c13acd4aaa81dfe6482a56d4a0680da8b8ea
Johnny <jonathanbc2012@gmail.com>
2024-01-08 21:30:25 +0100

    get countries and cities created

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { getCountries } from "./services/countries";
+import { getCities } from "./services/cities";
+
+export default function App() {
+  const [countries, setCountries] = useState([]);
+  const [cities, setCities] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const fetchedCountries = await getCountries();
+      const sortedCountries = [...fetchedCountries].sort((a, b) => {
+        return a.name.common.localeCompare(b.name.common);
+      });
+
+      setCountries(sortedCountries);
+    })();
+  }, []);
+
+  const countryHandler = async (e) =>
+    e.currentTarget.value && setCities(await getCities(e.currentTarget.value));
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <label>Selecciona un país: </label>
+        <select onChange={countryHandler}>
+          <option value="">Selecciona</option>
+          {countries.map((country) => (
+            <option key={country.cca2} value={country.cca2}>
+              {country.name.common}
+            </option>
+          ))}
+        </select>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+      {cities.length > 0 && (
+        <div>
+          <label>Elige una ciudad:</label>
+          <select onChange={countryHandler}>
+            {cities.map((city) => (
+              <option key={city.id} value={city.id}>
+                {city.name}
+              </option>
+            ))}
+          </select>
+        </div>
+      )}
+    </>
+  );
+}
